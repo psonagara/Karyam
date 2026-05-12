de96d85b12e6f62e86e0ca9f78615c3245e3fb55
psonagara <pareshssonagara@gmail.com>
2026-05-12 16:05:11 +0530

    Api Gateway

---
+++ KaryamGateway/src/main/java/com/karyam/gateway/KaryamGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.karyam.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaryamGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaryamGatewayApplication.class, args);
+	}
+
+}
---
+++ KaryamGateway/src/main/java/com/karyam/gateway/config/GatewayCorsConfig.java
@@ -0,0 +1,27 @@
+package com.karyam.gateway.config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GatewayCorsConfig {
+	
+	@Value("${karyam.cors.allowed-origins}")
+	private String allowedOrigin;
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Apply to all paths
+                        .allowedOrigins(allowedOrigin) // Specific origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
