11fa77563350c48a87eafa8a7d51ca8acdd8ab7e
psonagara <pareshssonagara@gmail.com>
2026-06-15 12:17:51 +0530

    AdminServer

---
+++ AdminServer/src/test/java/com/karyam/admin/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karyam.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
